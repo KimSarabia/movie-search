fa2b090f7fb6aaa6e830679f04cb66553910df19
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-03-01 08:06:20 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+"use strict";
+
+//get the movie object
+function movieLi(moviesObject){
+  return $('<li class="result" data-id="' + moviesObject.imdbID +
+'"><strong>' + moviesObject.Title + '</strong><em> (' + moviesObject.Year + ')</em></li>');
+}
+
+//get the movie poster
+function moviePoster(url){
+	return $('<img src="' +url+ '" alt="Movie Poster" />');
+}
+
+//get the movie info
+function getMovieDetails(id){
+	$.ajax({
+			url: "http://www.omdbapi.com/",
+			method: "get",
+			data: {"i": id, "callback": "movies"},
+			dataType: "jsonp",
+			jsonpCallback: "movies",
+			success: function(data) {
+				// alert("Something happened");
+				// console.log(data);
+				var posterUrl = data.Poster;
+				var moviePosterImg = moviePoster(posterUrl);
+				$('.movie-detail').empty().append(moviePosterImg);
+			}
+		});
+}
+
+//get the imdb  ID
+function getImdbID(){
+	var imdbID = $(this).attr("data-id");
+	getMovieDetails(imdbID);
+}
+
+// jQuery Main Function
+$(function() {
+	
+	$(".results").on("click", "li", getImdbID); 
+
+	$("form").on("submit", function(event) {
+		event.preventDefault();
+		var searchTerm = $("input").val()
+			, i = 0
+			, movieTitle
+		;
+		$.ajax({
+			url: "http://www.omdbapi.com/",
+			method: "get",
+			data: {"s": searchTerm, "callback": "movies"},
+			dataType: "jsonp",
+			jsonpCallback: "movies",
+			success: function(data) {
+				console.log(data);
+				var movies = data.Search;
+				$(".results").empty();
+				for (i = 0; i < movies.length; i++) {
+					movieTitle = movieLi(movies[i]);
+					$(".results").append(movieTitle);
+				}
+			}
+		});
+	});
+});
